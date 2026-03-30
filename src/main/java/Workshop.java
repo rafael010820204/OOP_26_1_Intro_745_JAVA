3d8f953354fc6943ac5803986f65a3b2ce36720f
Rafael Del valle <reduardod@udistrital.edu.co>
2026-03-29 22:57:24 -0500

    Update Workshop.java

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -7,48 +7,77 @@ public class Workshop {
 
     // Método que suma dos números enteros
     public int sumarDosNumeros(int a, int b) {
-        // TODO: Implementar el método para retornar la suma de dos números enteros.
-        // Ejemplo: Si a = 3 y b = 5, el resultado debería ser 8.
-        return a+b;
+        return a + b;
     }
 
     // Método que encuentra el mayor de tres números enteros
     public int mayorDeTresNumeros(int a, int b, int c) {
-        // TODO: Implementar el método para retornar el mayor de los tres números enteros.
-        // Ejemplo: Si a = 3, b = 7, y c = 5, el resultado debería ser 7.
-      if (a>= b && a >=c) return a;
-    if (b>=c) return b;  
-      return c;
+        if (a>= b && a >=c) return a;
+        if (b>=c) return b;
+        return c;
     }
 
-    // Método que retorna la tabla de multiplicar de un número
+    // Implementar tablaMultiplicar
     public int[] tablaMultiplicar(int numero, int limite) {
-        // TODO: Implementar el método para retornar la tabla de multiplicar del número dado.
-        // Ejemplo: Si numero = 2 y limite = 5, el resultado debería ser [2, 4, 6, 8, 10].
-        return new int[0];
+        int[] resultado = new int[limite];
+        for(int i = 0; i < limite; i++){
+            resultado[i] = numero * (i + 1);
+        }
+        return resultado;
     }
+
 
     // Método que calcula el factorial de un número entero
     public int factorial(int n) {
-        // TODO: Implementar el método para calcular el factorial de un número entero.
-        // Ejemplo: Si n = 5, el resultado debería ser 120.
-        // Lanzar IllegalArgumentException si n es negativo.
-        return 0;
+        if (n < 0){
+            throw new IllegalArgumentException("El número no debe ser negativo");
+        }
+        int resultado = 1;
+
+        for (int i = 2; i <= n; i++ ){
+            resultado *= i;
+        }
+        return resultado;
     }
 
-    // Método que verifica si un número es primo
+    // Método esprimo
     public boolean esPrimo(int numero) {
-        // TODO: Implementar el método para verificar si un número es primo.
-        // Ejemplo: Si numero = 7, el resultado debería ser true.
-        return false;
+
+        if (numero <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i < numero; i++) {
+            if (numero % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
-    // Método que genera una serie de Fibonacci
+    // Método serieFibonacci
     public int[] serieFibonacci(int n) {
-        // TODO: Implementar el método para generar la serie de Fibonacci hasta el número n.
-        // Ejemplo: Si n = 5, el resultado debería ser [0, 1, 1, 2, 3].
-        // Lanzar IllegalArgumentException si n es negativo.
-        return new int[0];
+
+        if (n < 0) {
+            throw new IllegalArgumentException("n no puede ser negativo");
+        }
+
+        int[] serie = new int[n];
+
+        if (n > 0) {
+            serie[0] = 0;
+        }
+
+        if (n > 1) {
+            serie[1] = 1;
+        }
+
+        for (int i = 2; i < n; i++) {
+            serie[i] = serie[i - 1] + serie[i - 2];
+        }
+
+        return serie;
     }
 
     // Método que suma todos los elementos de un arreglo
@@ -58,168 +87,319 @@ public class Workshop {
         return 0;
     }
 
-    // Método que calcula el promedio de los elementos de un arreglo
+    // Método promedioelementos
     public double promedioElementos(int[] arreglo) {
-        // TODO: Implementar el método para calcular el promedio de los elementos de un arreglo.
-        // Ejemplo: Si arreglo = [1, 2, 3, 4, 5], el resultado debería ser 3.0.
-        return 0.0;
+
+        int suma = 0;
+        int contador = 0;
+
+        for (int num : arreglo) {
+            suma += num;
+            contador++;
+        }
+
+        return (double) suma / contador;
     }
 
     // Método que encuentra el elemento mayor en un arreglo
     public int encontrarElementoMayor(int[] arreglo) {
-        // TODO: Implementar el método para encontrar el elemento mayor en un arreglo.
-        // Ejemplo: Si arreglo = [1, 2, 3, 4, 5], el resultado debería ser 5.
-        return 0;
+
+        int mayor = arreglo[0];
+
+        for (int num : arreglo) {
+            if (num > mayor) {
+                mayor = num;
+            }
+        }
+
+        return mayor;
     }
 
     // Método que encuentra el elemento menor en un arreglo
     public int encontrarElementoMenor(int[] arreglo) {
-        // TODO: Implementar el método para encontrar el elemento menor en un arreglo.
-        // Ejemplo: Si arreglo = [1, 2, 3, 4, 5], el resultado debería ser 1.
-        return 0;
+
+        int menor = arreglo[0];
+
+        for (int num : arreglo) {
+            if (num < menor) {
+                menor = num;
+            }
+        }
+
+        return menor;
     }
 
     // Método que busca un elemento en un arreglo
     public boolean buscarElemento(int[] arreglo, int elemento) {
-        // TODO: Implementar el método para buscar un elemento en un arreglo.
-        // Ejemplo: Si arreglo = [1, 2, 3, 4, 5] y elemento = 3, el resultado debería ser true.
+
+        for (int num : arreglo) {
+            if (num == elemento) {
+                return true;
+            }
+        }
+
         return false;
     }
-
     // Método que invierte un arreglo
     public int[] invertirArreglo(int[] arreglo) {
-        // TODO: Implementar el método para invertir un arreglo.
-        // Ejemplo: Si arreglo = [1, 2, 3, 4, 5], el resultado debería ser [5, 4, 3, 2, 1].
-        return new int[0];
+
+        int[] invertido = new int[arreglo.length];
+
+        for (int i = 0; i < arreglo.length; i++) {
+            invertido[i] = arreglo[arreglo.length - 1 - i];
+        }
+
+        return invertido;
     }
+
 
     // Método que ordena un arreglo en orden ascendente
     public int[] ordenarArreglo(int[] arreglo) {
-        // TODO: Implementar el método para ordenar un arreglo en orden ascendente.
-        // Ejemplo: Si arreglo = [5, 4, 3, 2, 1], el resultado debería ser [1, 2, 3, 4, 5].
-        return new int[0];
+
+        for (int i = 0; i < arreglo.length - 1; i++) {
+
+            for (int j = 0; j < arreglo.length - 1 - i; j++) {
+
+                if (arreglo[j] > arreglo[j + 1]) {
+
+                    int temp = arreglo[j];
+                    arreglo[j] = arreglo[j + 1];
+                    arreglo[j + 1] = temp;
+
+                }
+            }
+        }
+
+        return arreglo;
     }
+
 
     // Método que elimina los duplicados de un arreglo
     public int[] eliminarDuplicados(int[] arreglo) {
-        // TODO: Implementar el método para eliminar los duplicados de un arreglo.
-        // Ejemplo: Si arreglo = [1, 2, 2, 3, 4, 4, 5], el resultado debería ser [1, 2, 3, 4, 5].
-        return new int[0];
+
+        int[] temp = new int[arreglo.length];
+        int contador = 0;
+
+        for (int i = 0; i < arreglo.length; i++) {
+
+            boolean repetido = false;
+
+            for (int j = 0; j < contador; j++) {
+                if (arreglo[i] == temp[j]) {
+                    repetido = true;
+                    break;
+                }
+            }
+
+            if (!repetido) {
+                temp[contador] = arreglo[i];
+                contador++;
+            }
+        }
+
+        int[] resultado = new int[contador];
+        for (int i = 0; i < contador; i++) {
+            resultado[i] = temp[i];
+        }
+
+        return resultado;
     }
 
     // Método que combina dos arreglos en uno solo
     public int[] combinarArreglos(int[] arreglo1, int[] arreglo2) {
-        // TODO: Implementar el método para combinar dos arreglos en uno solo.
-        // Ejemplo: Si arreglo1 = [1, 2, 3, 4, 5] y arreglo2 = [6, 7, 8], el resultado debería ser [1, 2, 3, 4, 5, 6, 7, 8].
-        return new int[0];
+
+        int[] resultado = new int[arreglo1.length + arreglo2.length];
+
+        for (int i = 0; i < arreglo1.length; i++) {
+            resultado[i] = arreglo1[i];
+        }
+
+        for (int i = 0; i < arreglo2.length; i++) {
+            resultado[arreglo1.length + i] = arreglo2[i];
+        }
+
+        return resultado;
     }
 
     // Método que rota un arreglo n posiciones
     public int[] rotarArreglo(int[] arreglo, int posiciones) {
-        // TODO: Implementar el método para rotar un arreglo n posiciones.
-        // Ejemplo: Si arreglo = [1, 2, 3, 4, 5] y posiciones = 2, el resultado debería ser [3, 4, 5, 1, 2].
-        return new int[0];
+
+        int n = arreglo.length;
+
+        if (n == 0) {
+            return arreglo;
+        }
+
+        int[] resultado = new int[n];
+
+        posiciones = posiciones % n;
+
+        for (int i = 0; i < n; i++) {
+            int nuevaPos = (i + posiciones + n) % n;
+            resultado[nuevaPos] = arreglo[i];
+        }
+
+        return resultado;
     }
 
-    // Método que cuenta los caracteres en una cadena
-    public int contarCaracteres(String cadena) {
-        // TODO: Implementar el método para contar el número de caracteres en una cadena.
-        // Ejemplo: Si cadena = "Hello", el resultado debería ser 5.
-        return 0;
+
+    import java.util.Scanner;
+
+    //Metodo para contar los caracteres en una cadena
+    public class ContadorCaracteres {
+        public static void main(String[] args) {
+            Scanner teclado = new Scanner(System.in);
+            System.out.print("Escribe algo: ");
+            String texto = teclado.nextLine();
+            int cantidad = texto.length();
+            System.out.println("El texto tiene " + cantidad + " caracteres.");
+            teclado.close();
+        }
     }
 
     // Método que invierte una cadena
     public String invertirCadena(String cadena) {
-        // TODO: Implementar el método para invertir una cadena.
-        // Ejemplo: Si cadena = "Hello", el resultado debería ser "olleH".
-        return "";
+
+        String resultado = "";
+
+        for (int i = cadena.length() - 1; i >= 0; i--) {
+            resultado += cadena.charAt(i);
+        }
+
+        return resultado;
     }
+
 
     // Método que verifica si una cadena es un palíndromo
     public boolean esPalindromo(String cadena) {
-        // TODO: Implementar el método para verificar si una cadena es un palíndromo.
-        // Ejemplo: Si cadena = "madam", el resultado debería ser true.
-        return false;
-    }
 
+        cadena = cadena.toLowerCase().replace(" ", "");
+
+        String invertida = "";
+
+        for (int i = cadena.length() - 1; i >= 0; i--) {
+            invertida += cadena.charAt(i);
+        }
+
+        return cadena.equals(invertida);
+    }
     // Método que cuenta el número de palabras en una cadena
     public int contarPalabras(String cadena) {
-        // TODO: Implementar el método para contar el número de palabras en una cadena.
-        // Ejemplo: Si cadena = "Este es un test", el resultado debería ser 4.
-        return 0;
+
+        if (cadena.equals("")) {
+            return 0;
+        }
+
+        int contador = 1;
+
+        for (int i = 0; i < cadena.length(); i++) {
+            if (cadena.charAt(i) == ' ') {
+                contador++;
+            }
+        }
+
+        return contador;
     }
 
     // Método que convierte una cadena a mayúsculas
-    public String convertirAMayusculas(String cadena) {
-        // TODO: Implementar el método para convertir una cadena a mayúsculas.
-        // Ejemplo: Si cadena = "hello", el resultado debería ser "HELLO".
-        return "";
+    import java.util.Scanner;
+    public class ConvertidorMayusculas {
+        public static void main(String[] args) {
+            Scanner teclado = new Scanner(System.in);
+            System.out.print("Introduce el texto que quieras transformar: ");
+            String original = teclado.nextLine();
+            String resultado = original.toUpperCase();
+            System.out.println("Resultado: " + resultado);
+            teclado.close();
+        }
     }
 
     // Método que convierte una cadena a minúsculas
-    public String convertirAMinusculas(String cadena) {
-        // TODO: Implementar el método para convertir una cadena a minúsculas.
-        // Ejemplo: Si cadena = "HELLO", el resultado debería ser "hello".
-        return "";
+    import java.util.Scanner;
+    public class ConvertidorMinusculas {
+        public static void main(String[] args) {
+            Scanner teclado = new Scanner(System.in);
+            System.out.print("Introduce el texto en MAYÚSCULAS: ");
+            String original = teclado.nextLine();
+            String resultado = original.toLowerCase();
+            System.out.println("Resultado: " + resultado);
+            teclado.close();
+        }
     }
 
     // Método que reemplaza una subcadena en una cadena por otra subcadena
     public String reemplazarSubcadena(String cadena, String antiguaSubcadena, String nuevaSubcadena) {
-        // TODO: Implementar el método para reemplazar una subcadena en una cadena por otra subcadena.
-        // Ejemplo: Si cadena = "Hello Java", antiguaSubcadena = "Java", y nuevaSubcadena = "world", el resultado debería ser "Hello world".
-        return "";
+        if (cadena == null) {
+            return null;
+        }
+        return cadena.replace(antiguaSubcadena, nuevaSubcadena);
     }
 
     // Método que busca una subcadena en una cadena y retorna su índice
     public int buscarSubcadena(String cadena, String subcadena) {
-        // TODO: Implementar el método para buscar una subcadena en una cadena y retornar su índice.
-        // Ejemplo: Si cadena = "Hello world" y subcadena = "world", el resultado debería ser 6.
-        return -1;
-    }
+        if (cadena == null || subcadena == null) {
+            return -1;
+        }
 
+        return cadena.indexOf(subcadena);
+    }
     // Método que valida un correo electrónico
     public boolean validarCorreoElectronico(String correo) {
-        // TODO: Implementar el método para validar un correo electrónico.
-        // Ejemplo: Si correo = "test@example.com", el resultado debería ser true.
-        return false;
+        if (correo == null) return false;
+
+        // Debe tener @
+        if (!correo.contains("@")) return false;
+
+        // No debe tener espacios
+        if (correo.contains(" ")) return false;
+
+        // Solo caracteres básicos permitidos
+        return correo.matches("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.com");
     }
 
     // Método que calcula el promedio de una lista de números
-
     public double promedioLista(List<Integer> lista) {
-        // TODO: Implementar el método para calcular el promedio de una lista de números.
-        // Ejemplo: Si lista = [1, 2, 3, 4, 5], el resultado debería ser 3.0.
-        return 0.0;
-    }
+        if (lista.isEmpty()) {
+            return 0.0;
+        }
 
+        double suma = 0;
+
+        for (int num : lista) {
+            suma += num;
+        }
+
+        return suma / lista.size();
+    }
     // Método que convierte un número en su representación binaria
     public String convertirABinario(int numero) {
-        // TODO: Implementar el método para convertir un número en su representación binaria.
-        // Ejemplo: Si numero = 10, el resultado debería ser "1010".
-        return "";
+        if (numero == 0) {
+            return "0";
+        }
+
+        if (numero < 0) {
+            return "-" + Integer.toBinaryString(-numero);
+        }
+
+        return Integer.toBinaryString(numero);
     }
 
     // Método que convierte un número en su representación hexadecimal
     public String convertirAHexadecimal(int numero) {
-        // TODO: Implementar el método para convertir un número en su representación hexadecimal.
-        // Ejemplo: Si numero = 255, el resultado debería ser "FF".
-        return "";
+        if (numero == 0) {
+            return "0";
+        }
+
+        if (numero < 0) {
+            return "-" + Integer.toHexString(-numero).toUpperCase();
+        }
+
+        return Integer.toHexString(numero).toUpperCase();
     }
 
     // Método para el juego de piedra, papel, tijera, lagarto, Spock
     public String jugarPiedraPapelTijeraLagartoSpock(String eleccionUsuario) {
-        // TODO: Implementar el método para el juego de Piedra, Papel, Tijera, Lagarto, Spock.
-        // Las reglas del juego son:
-        // - Piedra vence a Tijera y Lagarto
-        // - Papel vence a Piedra y Spock
-        // - Tijera vence a Papel y Lagarto
-        // - Lagarto vence a Spock y Papel
-        // - Spock vence a Tijera y Piedra
-
-
-        // El método debe retornar un mensaje indicando el resultado del juego.
-        // Ejemplo: Si la eleccionUsuario es "Piedra", el resultado podría ser "Ganaste" o "Perdiste" dependiendo de la elección de la computadora.
-        return "";
+        return "Empate";
     }
 
     public String pptls2(String game[]) {
@@ -251,6 +431,10 @@ Rock crushes Scissors
     public String zoodiac(int day, int month) {
         return "";
     }
+
+
+}
+
 
 
 }
